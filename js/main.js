4144bca5f2971df51a6b3323e8b1253772e43d4d
ShantonWaldien <waldienshanton@gmail.com>
2019-01-17 15:45:37 +0200

    added stats counter using jquery

--- js/main.js
+++ js/main.js
@@ -18,15 +18,88 @@ function topFunction() {
 
 /**** Stats Counter Function *****/
 
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 2000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+// $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 2000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
 
+// Trigger Stat Counter
+
+// $(document).on('scroll', function() {
+
+//     if( $(this).scrollTop() >= $('.counter-section').position().top ){
+
+//         $('.count').each(function () {
+//             $(this).prop('Counter',0).animate({
+//                 Counter: $(this).text()
+//             }, {
+//                 duration: 2000,
+//                 easing: 'swing',
+//                 step: function (now) {
+//                     $(this).text(Math.ceil(now));
+//                 }
+//             });
+//         });
+//     }
+// });
+
+// function elementInView(elem){
+//     return $(window).scrollTop() < $(elem).offset().top + $(elem).height() ;
+//   };
+  
+//   $(window).scroll(function(){
+//     if (elementInView($('.counter-section')))
+//     //fire at will!
+
+//     $('.count').each(function () {
+//         $(this).prop('Counter',0).animate({
+//             Counter: $(this).text()
+//         }, {
+//             duration: 2000,
+//             easing: 'swing',
+//             step: function (now) {
+//                 $(this).text(Math.ceil(now));
+//             }
+//         });
+//     });
+//   });
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
